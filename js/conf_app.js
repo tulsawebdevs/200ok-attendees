eaacb2a7752a243be0ba342490a3f13d5538564a
jdungan <johnadungan@gmail.com>
2013-09-25 21:32:44 -0500

    change padding

--- js/conf_app.js
+++ js/conf_app.js
@@ -68,7 +68,7 @@ angular.module('_200OK.controllers',[])
             var nb =new L.LatLngBounds([
                 $scope.venue_latlng,
                 $scope.map.user_position]);
-            $scope.map.fitBounds(nb,{paddingTopLeft:[50,0],paddingBottomRight:[0,75]})
+            $scope.map.fitBounds(nb,{paddingTopLeft:[0,75],paddingBottomRight:[0,100]})
         };
         
         $scope.slide_show = function($event){
